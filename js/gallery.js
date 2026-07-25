cbc881f7ccde5190300654b6df5c838e1b50a394
aridwansyah106-ops <aridwansyah106@gmail.com>
2026-07-26 02:26:18 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,28 @@
+fetch("data/gallery.json")
+.then(response => response.json())
+.then(data => {
 
+    const gallery = document.getElementById("gallery");
+
+    gallery.innerHTML = "";
+
+    data.photos.forEach(photo => {
+
+        gallery.innerHTML += `
+
+        <div class="col-lg-4 col-md-6 col-6">
+
+            <div class="gallery-card">
+
+                <img src="${photo.image}"
+                     class="gallery-image">
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+});
