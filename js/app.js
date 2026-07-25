0307be70d421d6d94772ebeac425ec2bf37c6bd9
aridwansyah106-ops <aridwansyah106@gmail.com>
2026-07-26 02:23:58 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+fetch("data/event.json")
 
+.then(res=>res.json())
+
+.then(data=>{
+
+    document.getElementById("coupleName").innerHTML=data.couple;
+
+    document.querySelector(".date").innerHTML=data.date;
+
+    document.getElementById("photoLink").href=data.photoLink;
+
+    document.getElementById("gifLink").href=data.gifLink;
+
+});
